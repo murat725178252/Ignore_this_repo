a845d6a88b5bd21c52012043ee69638f8ddae4d7
YSP <141450344+murat725178252@users.noreply.github.com>
2024-03-20 19:41:53 +0300

    :3

---
+++ Homework/Homework1-Password.py
@@ -0,0 +1,73 @@
+import os
+os.system("pip install colorama")
+import colorama
+import random
+
+PasswordChars = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "@",
+    "-",
+    "_",
+    "*",
+    "?",
+    "!",
+    "%",
+    "^",
+    ".",
+    "#",
+    "$",
+    ">",
+    "<",
+    "/",
+    "&",
+    "+",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+]
+Password = ""
+
+def CreatePassword(Len: int):
+    global Password
+    for _ in range(PasswordLen):
+
+        i = random.choice(PasswordChars)
+
+        Password = Password + i
+
+print(colorama.Fore.YELLOW + "---- Password Creator ----")
+while True:
+    PasswordLen = int(input(colorama.Fore.YELLOW + "Enter Password Length >>>"))
+    CreatePassword(PasswordLen)
+    print("Password : '" + Password + "'")
